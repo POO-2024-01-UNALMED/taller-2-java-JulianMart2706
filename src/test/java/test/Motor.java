d788d59cd84f9c931aef35a95950a4aed2641b70
JulianMart2706 <julmartinezc@unal.edu.co>
2024-02-19 18:56:10 -0500

    Commit Julian

---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Motor {
+	int numeroCilindros;
+	String tipo;
+	int registro;
+	
+	void cambiarRegistro(int nuevoRegistro){	
+		this.registro = nuevoRegistro;
+	}
+	
+	void asignarTipo(String nuevoTipo){
+		if(nuevoTipo == "electrico" || nuevoTipo == "electrico") {
+			this.tipo = nuevoTipo;
+		}
+	}
+
+}
